e0c4491fae7383901b12158221594b7bd1e8ec52
Jerome Velut <jerome.velut@kitware.com>
2015-09-23 15:19:50 +0200

    Added logging macro definition

---
+++ lilthumb.h
@@ -0,0 +1,23 @@
+#ifndef lilthumb
+#define lilthumb
+
+#include <ctime>
+namespace lilthumb{
+std::string timeString()
+{
+  time_t rawtime;
+  struct tm * timeinfo;
+  char buffer[80];
+
+  time (&rawtime);
+  timeinfo = localtime(&rawtime);
+
+  strftime(buffer,80,"%d-%m-%Y %I:%M:%S",timeinfo);
+  std::string str(buffer);
+
+  return str;
+}
+}
+
+#define logger(message) std::cerr << lilthumb::timeString() << " | "<< message << std::endl;
+#endif
